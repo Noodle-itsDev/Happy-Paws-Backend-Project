c056e8d896d0b4191b5be62b8c222896945c9fa2
KJoTechTalent <acortec.johana@gmail.com>
2024-07-11 21:37:00 +0200

    Constante de Seguridad

--- src/main/java/com/http/happypaws/security/ConstantesSeguridad.java
+++ src/main/java/com/http/happypaws/security/ConstantesSeguridad.java
@@ -1,5 +1,9 @@
 package com.http.happypaws.security;
 
-public class ConstantesSeguridad {
 
+public class ConstantesSeguridad {
+	
+	//Defino constantes de seguridad
+	public static final long JWT_EXPIRATION_TOKEN = 300000000;
+	public static final String JWT_FIRMA = "firma";
 }
