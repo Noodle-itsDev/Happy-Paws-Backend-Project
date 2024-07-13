8c6d4be3b2e20405b680dd49939b17041ed775a5
Johana Almeida Cortecero <acortec.johana@gmail.com>
2024-07-13 19:13:22 +0200

    Modificaciones estéticas en código

--- src/main/java/com/http/happypaws/security/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/http/happypaws/security/JwtAuthenticationEntryPoint.java
@@ -1,11 +1,9 @@
 package com.http.happypaws.security;
 
 import java.io.IOException;
-
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
-
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -17,7 +15,6 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint{
 	public void commence(HttpServletRequest request, HttpServletResponse response,AuthenticationException authException) throws IOException, ServletException{
 		response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
 	}
-
 		
 }
 
