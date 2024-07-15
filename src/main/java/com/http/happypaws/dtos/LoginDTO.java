6c6659cf3bc519dbaea77050d20aedb03746880c
KJoTechTalent <acortec.johana@gmail.com>
2024-07-15 22:51:42 +0200

    Creación AuthController
    Se ajustan DTOS, se elimina Lombok por incompatibilidad con métodos lazy. Se crean controlladores Auth.

--- src/main/java/com/http/happypaws/dtos/LoginDTO.java
+++ src/main/java/com/http/happypaws/dtos/LoginDTO.java
@@ -1,12 +1,38 @@
 package com.http.happypaws.dtos;
 
-import lombok.Data;
-
-@Data
-//Creo los getters y los setters mediante Lombok
 
 public class LoginDTO {
 	
 	private String username;
 	private String password;
+	
+	public LoginDTO() {
+
+	}
+
+	public LoginDTO(String username, String password) {
+		super();
+		this.username = username;
+		this.password = password;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
+	
 }
